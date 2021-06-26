906b37abd009a69c44f4bcdc072abab89eb174cd
ZakAP <phillipszak@hotmail.com>
2021-06-26 14:24:17 -0300

    divisão basica do projeto e adição das partes mais simples

---
+++ src/components/UserCard/UserCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserCard(props) {
+    return (
+      <div>
+        <h2>{props.login}</h2>
+        <img src={props.avatar} width="300" height="200" />
+        <p>{props.name}</p>
+      </div>
+    );
+  }
+  
+export default UserCard;
